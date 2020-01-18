07b6e0993fd542a1ca4b26f70fe5c3efafc14100
Rafael Montes <rafaelmontes1997@gmail.com>
2020-01-17 20:29:35 -0500

    Was able to make the Player jump but can do multiple jumps

--- Sophomore Project/Assets/Scripts/PlayerController.cs
+++ Sophomore Project/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@
  */
 
 using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -16,6 +17,7 @@ public class PlayerController : MonoBehaviour
     public float jumpSpeed = 10.0f;
     public float horizontalInput;
     public float verticalInput;
+    private Rigidbody2D rigidbody2d;
 
 
     // Start is called before the first frame update
@@ -24,13 +26,21 @@ public class PlayerController : MonoBehaviour
         
     }
 
+    private void Awake()
+    {
+        rigidbody2d = transform.GetComponent<Rigidbody2D>();
+    }
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Console.WriteLine("Hello");
+            float jumpVelocity = 10f;
+            rigidbody2d.velocity = Vector2.up * jumpVelocity;
+        }
         horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
-        transform.Translate(Vector3.up * Time.deltaTime * speed * verticalInput);
         
 
     }
