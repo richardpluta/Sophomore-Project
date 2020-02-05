79f216b95696f428fce92cd16fcb8b979177fb10
Rafael Montes <rafytafy@Rafaels-MacBook-Pro.local>
2020-02-05 18:43:48 -0500

    Created PlatformMove.cs

---
+++ Sophomore Project/Assets/Scripts/PlatformAttach.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformAttach : MonoBehaviour
+{
+   public GameObject player;
+
+   private void OnTriggerEnter(CircleCollider2D collider)
+	{
+		if (collider.gameObject == player)
+		{
+			player.transform.parent = transform;
+		}
+	}
+
+	private void OnTriggerExit(CircleCollider2D collider)
+	{
+		if (collider.gameObject == player)
+		{
+			player.transform.parent = null;
+		}
+	}
+}
---
+++ Sophomore Project/Assets/Scripts/PlatformMove.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMove : MonoBehaviour
+{
+    private float dirX;
+    private bool moveRight = true;
+    private bool moveUp = true;
+
+    public GameObject player;
+    public float moveSpeed = 3f;
+    public float distanceHorizontal;
+    public float distanceVertical;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Horizontal();
+        Vertical();
+    }
+
+    void Horizontal()
+    {
+        if (transform.position.x > distanceHorizontal)
+            moveRight = false;
+        if (transform.position.x < -distanceHorizontal)
+            moveRight = true;
+
+        if (moveRight)
+            transform.position = new Vector2(transform.position.x + (moveSpeed * Time.deltaTime), transform.position.y);
+        else
+            transform.position = new Vector2(transform.position.x - (moveSpeed * Time.deltaTime), transform.position.y);
+    }
+
+    private void Vertical()
+    {
+        if (transform.position.y > distanceVertical)
+            moveUp = false;
+        if (transform.position.y < -distanceVertical)
+            moveUp = true;
+
+        if (moveUp)
+            transform.position = new Vector2(transform.position.x, transform.position.y + (moveSpeed * Time.deltaTime));
+        else
+            transform.position = new Vector2(transform.position.x, transform.position.y - (moveSpeed * Time.deltaTime));
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject == player)
+        {
+            player.transform.parent = transform;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject == player)
+        {
+            player.transform.parent = null;
+        }
+    }
+}
