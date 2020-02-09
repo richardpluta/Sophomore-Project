956398e3d02bac0ca6cd3ab8a32e4a97f9b701d5
Rafael Montes <rafaelmontes1997@gmail.com>
2020-02-09 15:33:26 -0500

    Added comments to PlatformMove.cs

--- Sophomore Project/Assets/Scripts/PlatformMove.cs
+++ Sophomore Project/Assets/Scripts/PlatformMove.cs
@@ -2,16 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+    Rafael Montes
+    2/9/2020
+    This script will make the platforms move and will allow player to ride on platform.
+*/
 public class PlatformMove : MonoBehaviour
 {
-    private float dirX;
     private bool moveRight = true;
     private bool moveUp = true;
 
     public GameObject player;
     public bool MoveHorizontal;
     public bool MoveVertical;
-    public float moveSpeed = 3f;
+    public float moveSpeed;
     public float distanceHorizontal;
     public float distanceVertical;
 
@@ -26,6 +30,7 @@ public class PlatformMove : MonoBehaviour
             Vertical();
     }
 
+    //Moves platform right to left 
     void Horizontal()
     {
         if (transform.position.x > distanceHorizontal)
@@ -38,7 +43,8 @@ public class PlatformMove : MonoBehaviour
         else
             transform.position = new Vector2(transform.position.x - (moveSpeed * Time.deltaTime), transform.position.y);
     }
-
+    
+    //Moves platform up and down 
     private void Vertical()
     {
         if (transform.position.y > distanceVertical)
@@ -52,6 +58,7 @@ public class PlatformMove : MonoBehaviour
             transform.position = new Vector2(transform.position.x, transform.position.y - (moveSpeed * Time.deltaTime));
     }
 
+    //Will make player "stick" when touching platform
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject == player)
@@ -60,6 +67,7 @@ public class PlatformMove : MonoBehaviour
         }
     }
 
+    //Will make player "unstick" when leaving platform 
     void OnCollisionExit2D(Collision2D collision)
     {
         if(collision.gameObject == player)
