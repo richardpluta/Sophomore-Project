5915336197088d6f6805820995fa887200cf476d
Rafael Montes <rafaelmontes1997@gmail.com>
2020-02-10 09:39:23 -0500

    Added Respawn.cs with code

---
+++ Sophomore Project/Assets/Scripts/Respawn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public GameObject respawnPoint;
+    public GameObject player;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject == player)
+        {
+            player.transform.position = respawnPoint.transform.position;
+        }
+    }
+}
