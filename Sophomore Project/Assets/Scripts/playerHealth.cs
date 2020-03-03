457a6c299e1c0c96b254bdaba9df14dc92dc5acd
DngerCrazy <maxdawe17@gmail.com>
2020-03-03 12:28:09 -0500

    Added Health UI
    I added a script to control the health UI, still need to work on the actual logic of the health though.

---
+++ Sophomore Project/Assets/Scripts/playerHealth.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerHealth : MonoBehaviour
+{
+    public int health;
+    public int maxHealth;
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    void Update()
+    {
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < health)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+            if (i < maxHealth)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+    }
+}
