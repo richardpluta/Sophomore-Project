a8069ab783bcbb38a8c3d68a94f0a011e138ae3c
Dan <dsumindan@oakland.edu>
2020-03-22 22:41:14 -0400

    Invincibility timer, A* fix, checkpoint/spike no longer blocked by raycast

--- Sophomore Project/Assets/Scripts/playerHealth.cs
+++ Sophomore Project/Assets/Scripts/playerHealth.cs
@@ -7,6 +7,11 @@ using System;
 
 public class playerHealth : MonoBehaviour
 {
+    [SerializeField]
+    private float invincibilityDurationSeconds;
+    [SerializeField]
+    private float delayBetweenInvincibilityFlashes;
+
     public int health;
     public int maxHealth;
     public int EnemyDamage;
@@ -14,15 +19,13 @@ public class playerHealth : MonoBehaviour
     public Image[] hearts;
     public Sprite fullHeart;
     public Sprite emptyHeart;
-
     private CharacterController2D player;
-
+    private bool isInvincible = false;
 
     void Start()
     {
         health = maxHealth;
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterController2D>();
-
     }
  
     void Update()
@@ -62,22 +65,43 @@ public class playerHealth : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter2D(Collider2D obj)
-    {
-        if (obj.tag == "Enemy")
-        {
-            damagePlayer(1);
-        }
+    private IEnumerator coroutine;
 
-        if (obj.tag == "Spikes")
+    private void OnTriggerStay2D(Collider2D obj)
+    {
+        if (obj.tag == "Enemy" || obj.tag == "Spikes")
         {
             damagePlayer(1);
         }
+        
     }
 
     public void damagePlayer(int dmg)
     {
+        if (isInvincible) return;
+
         health -= dmg;
+
+        StartCoroutine(BecomeTemporarilyInvincible());
     }
+
+    private IEnumerator BecomeTemporarilyInvincible()
+    {
+        Debug.Log("Player turned invincible!");
+        isInvincible = true;
+
+        // Flash on and off for roughly invincibilityDurationSeconds seconds
+        for (float i = 0; i < invincibilityDurationSeconds; i += delayBetweenInvincibilityFlashes)
+        {
+            player.GetComponent<SpriteRenderer>().enabled = !(player.GetComponent<SpriteRenderer>().enabled);
+            yield return new WaitForSeconds(delayBetweenInvincibilityFlashes);
+        }
+
+        player.GetComponent<SpriteRenderer>().enabled = true;
+
+        Debug.Log("Player is no longer invincible!");
+        isInvincible = false;
+    }
+
 
 }
