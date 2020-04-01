efe10892db7e44a1a7e65f118401fdc589a850e8
Richast <39656777+Richast@users.noreply.github.com>
2020-03-31 23:26:44 -0400

    Removes unused enemy code

--- Sophomore Project/Assets/Scripts/EnemyController.cs
+++ Sophomore Project/Assets/Scripts/EnemyController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using Pathfinding;
 
 public class EnemyController : MonoBehaviour
 {
@@ -22,7 +21,6 @@ public class EnemyController : MonoBehaviour
     private SpriteRenderer Renderer;
     private CapsuleCollider2D Collider;
     private Rigidbody2D Rigidbody2D;
-    private Seeker Seeker;
 
     private Transform Player;
 
@@ -31,14 +29,7 @@ public class EnemyController : MonoBehaviour
     private Vector2 Velocity = Vector2.zero;
     private int Health;
 
-    private bool UsingPath;
-    [SerializeField] private float NextWaypointDistance;
-    private Path Path;
-    private int CurrentWaypoint;
-
     private bool SeenPlayer;
-    
-
 
     public UnityEvent OnLandEvent;
 
@@ -49,13 +40,11 @@ public class EnemyController : MonoBehaviour
         Renderer = GetComponent<SpriteRenderer>();
         Collider = GetComponent<CapsuleCollider2D>();
         Rigidbody2D = GetComponent<Rigidbody2D>();
-        Seeker = GetComponent<Seeker>();
 
         Player = GameObject.Find("Player").transform;
 
         Health = MaxHealth;
 
-        UsingPath = false;
         SeenPlayer = false;
         if (OnLandEvent == null)
             OnLandEvent = new UnityEvent();
@@ -82,77 +71,24 @@ public class EnemyController : MonoBehaviour
    
         if (SeenPlayer)
         {
-            //Debug.Log("Enemy is engaging player");
             //move towards player on X, jump if something is in its way
             Vector2 offset = Player.position - transform.position;
             float horizontalMove = offset.normalized.x * Speed;
 
-            //Debug.Log("Offset: " + offset + "\nHorizontal: " + horizontalMove);
             RaycastHit2D hit = Physics2D.Raycast(GroundCheck.position, -GroundCheck.right, .25f, LayerMask.GetMask("Ground"));
             bool jump = (hit.transform != null) || (offset.y > .5f);
-
-            //bool jump = (Physics2D.Raycast(transform.position, -transform.right, .25f, LayerMask.GetMask("Ground")) != null);
-            Debug.Log("Jump: " + jump);
 
             Move(horizontalMove * Time.fixedDeltaTime, jump);
         } else if (Vector2.Distance(Player.position, transform.position) <= DetectionRange)
         {
-            Debug.Log("Enemy in range");
             RaycastHit2D hit = Physics2D.Raycast(transform.position, Player.position - transform.position, 10f, LayerMask.GetMask("Default", "Ground"));
-            Debug.Log("Hit: " + hit.transform);
             SeenPlayer = hit.transform == Player;
 
         }
-        /*
-        //Check if player is in range or not
-        if (!UsingPath)
-        {
-            Debug.Log("Enemy not following path");
-            if (Vector2.Distance(Player.position, transform.position) <= DetectionRange)
-            {
-                Debug.Log("Player within range");
-                UsingPath = true;
-                this.Seeker.StartPath(this.Rigidbody2D.position, this.Player.position, OnPathComplete);
-            }
-            else
-            {
-
-            }
-        } else if (this.Path != null) 
-        {
-            Debug.Log("Path is not null");
-            if (CurrentWaypoint >= this.Path.vectorPath.Count)
-            {
-                Debug.Log("Current waypoint is outside of the path");
-                this.Path = null;
-                CurrentWaypoint = 0;
-                UsingPath = false;
-                return;
-            }
-
-            Vector2 direction = ((Vector2)this.Path.vectorPath[CurrentWaypoint] - this.Rigidbody2D.position).normalized;
-            Vector2 force = direction * Speed * Time.deltaTime;
-
-            Debug.Log("Current waypoint: " + this.Path.vectorPath[CurrentWaypoint] + "Current position: " + this.Rigidbody2D.position + "Direction:" + direction + "\nForce: " + force);
-
-            this.Rigidbody2D.AddForce(force);
-
-            float distance = Vector2.Distance(this.Rigidbody2D.position, this.Path.vectorPath[CurrentWaypoint]);
-
-            if (distance < NextWaypointDistance)
-            {
-                CurrentWaypoint++;
-            }
-
-        } 
-        */
     }
 
     private void Move(float move, bool jump)
     {
-        Debug.Log("Move called, grounded: " + Grounded + " move: " + move);
-
-
         // Move the character by finding the target velocity
         Vector2 targetVelocity = new Vector2(move * 10f, Rigidbody2D.velocity.y);
         // And then smoothing it out and applying it to the character
@@ -191,17 +127,5 @@ public class EnemyController : MonoBehaviour
 
         if (Health <= 0)
             Kill();
-    }
-
-    void OnPathComplete(Path p)
-    {
-        if (!p.error)
-        {
-            Debug.Log("Path generated");
-            this.Path = p;
-            CurrentWaypoint = 0;
-        }
-    }
-
-    
+    }    
 }
