79d9b655b7daaa63d8b9a9292e3b2cb350794575
Richast <39656777+Richast@users.noreply.github.com>
2020-04-08 14:54:44 -0400

    Starts pausing

---
+++ Sophomore Project/Assets/Scripts/Pause.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+static public class Pause
+{
+    static public bool Paused;
+
+    static public void PauseGame()
+    {
+        Paused = true;
+
+       
+    }
+}
